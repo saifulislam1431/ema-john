7cd10d46fa9006c5a26d9ddc4fd843d1eb2b9b80
Saiful Islam <sakibbhaiya143@gmail.com>
2023-03-27 02:34:49 +0600

    Shopping cart btn fixed

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,23 +3,20 @@ import './Product.css';
 
 const Product = (props) => {
     console.log(props.product);
-    const{img, name, price, ratings, seller} = props.product;
+    const { img, name, price, ratings, seller } = props.product;
     return (
         <div className='product'>
             <div className='img'>
-            <img src={img} alt="Product Image" className='product-img'/>
+                <img src={img} alt="Product Image" className='product-img' />
             </div>
             <div className='details'>
-            <h6 className='pro-name'>{name}</h6>
-            <p className='price'>Price: ${price}</p>
-            <p className='manufacture'>Manufacture: {seller}</p>
-            <p className='rat'>Rating: {ratings} star</p>
+                <h6 className='pro-name'>{name}</h6>
+                <p className='price'>Price: ${price}</p>
+                <p className='manufacture'>Manufacture: {seller}</p>
+                <p className='rat'>Rating: {ratings} star</p>
             </div>
-            <div className='cart-foot'>
-                <div>
-                <p className='foot-text'>Add to Cart</p>
-                </div>
-
+            <div>
+                <button className='foot-btn'>Add to Cart</button>
             </div>
         </div>
     );
