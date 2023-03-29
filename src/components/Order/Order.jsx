f65275ea21c8c43df32f50830e4b2de25f965b87
Saiful Islam <sakibbhaiya143@gmail.com>
2023-03-29 23:24:58 +0600

    retrieve product from stored cart and set it to cart state

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -5,22 +5,52 @@ import Product from '../Product/Product';
 import './Order.css'
 
 const Order = () => {
+    // useState from loaded data
     const[products,setProducts] = useState([]);
+    // useState for  passing data to Order review
     const[cart,setCart] = useState([]);
 
+    // Loading data from api
     useEffect(()=>{
         fetch('products.json')
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
+
+
+    // Loading data from api by stored local storage id
     useEffect(()=>{
       const storedCart = getShoppingCart()
-      
-    },[])
+      const updateCart = [];
 
+    //   Get id from local storage.
+      for(const id in storedCart){
+
+        // find Product by id from products.
+        const savedProducts = products.find(product=> product.id === id)
+        
+       if(savedProducts){
+         // Get quantity of the product
+         const quantity = storedCart[id];
+
+         // Set quantity of the product
+         savedProducts.quantity = quantity;
+
+        //  add the save product to the update cart
+        updateCart.push(savedProducts);
+       }
+       
+      }
+    //  Set the cart
+      setCart(updateCart);
+      
+    },[products])
+
+    // Passing data to review cart
     const handleShopCart = (product) =>{
        const newCart = [...cart, product]
        setCart(newCart)
+    //    store data in local storage
        addToDb(product.id)
         
     }
